75755aecc7037ffd3e9a65083a124440ff1cb66b
Dominik D. Geyer <dominik.geyer@holdingnuts.net>
2008-12-13 00:25:25 +0000

    first steps on client; test of non-blocking read of windows-stdin
    git-svn-id: http://svn.code.sf.net/p/holdingnuts/code@94 00c8e4f2-bc05-11dd-9892-7df5a783e2f7

--- trunk/src/client/pclient_t.cpp
+++ trunk/src/client/pclient_t.cpp
@@ -21,6 +21,11 @@
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
+#include <ctime>
+
+#if not defined(PLATFORM_WINDOWS)
+# include <signal.h>
+#endif
 
 #include <vector>
 #include <string>
@@ -34,28 +39,9 @@
 
 using namespace std;
 
-typedef enum {
-	Collecting=1,
-	Playing
-} gamestate;
+char server_address[256];
 
-typedef enum {
-	Settings=0x1,
-	Foyer=0x2,
-	Table=0x4
-} snaptype;
-
-
-socktype fdset_get_descriptor(fd_set *fds)
-{
-        socktype i = 0;
-
-        while(!FD_ISSET(i, fds))
-                i++;
-
-        return i;
-}
-
+#if 0
 int send_msg(socktype sock, const char *msg)
 {
 	const int bufsize = 1024;
@@ -72,26 +58,8 @@ int send_msg(socktype sock, const char *msg)
 }
 
 
-void Tokenize(const string& str, vector<string>& tokens, const string& delimiters = " ")
-{
-	// Skip delimiters at beginning.
-	string::size_type lastPos = str.find_first_not_of(delimiters, 0);
-	// Find first "non-delimiter".
-	string::size_type pos     = str.find_first_of(delimiters, lastPos);
-	
-	while (string::npos != pos || string::npos != lastPos)
-	{
-		// Found a token, add it to the vector.
-		tokens.push_back(str.substr(lastPos, pos - lastPos));
-		// Skip delimiters.  Note the "not_of"
-		lastPos = str.find_first_not_of(delimiters, pos);
-		// Find next "non-delimiter"
-		pos = str.find_first_of(delimiters, lastPos);
-	}
-}
-
 // returns zero if no cmd was found or no bytes remaining after exec
-int client_parsebuffer(clientcon *client)
+int server_parsebuffer(clientcon *client)
 {
 	//dbg_print("clientsock", "(%d) parse (bufferlen=%d)", client->sock, client->buflen);
 	
@@ -115,8 +83,8 @@ int client_parsebuffer(clientcon *client)
 		memcpy(cmd, client->msgbuf, found_nl);
 		cmd[found_nl] = '\0';
 		
-		//dbg_print("clientsock", "(%d) command: '%s' (len=%d)", client->sock, cmd, found_nl);
-		client_execute(client, cmd);
+		dbg_print("clientsock", "(%d) command: '%s' (len=%d)", client->sock, cmd, found_nl);
+		//server_execute(client, cmd);
 		
 		// move the rest to front
 		memmove(client->msgbuf, client->msgbuf + found_nl + 1, client->buflen - (found_nl + 1));
@@ -128,28 +96,21 @@ int client_parsebuffer(clientcon *client)
 	else
 		return 0;
 }
+#endif
 
-int client_handle(socktype sock)
+int server_handle(socktype sock)
 {
 	char buf[1024];
 	int bytes;
-	
-	errno = 0;   // workaround
 	
 	// return early on client close/error
 	if ((bytes = socket_read(sock, buf, sizeof(buf))) <= 0)
 		return bytes;
 	
 	
-	//dbg_print("clientsock", "(%d) DATA len=%d", sock, bytes);
+	dbg_print("connectsock", "(%d) DATA len=%d", sock, bytes);
 	
-	clientcon *client = get_client_by_sock(sock);
-	if (!client)
-	{
-		dbg_print("clientsock", "(%d) error: no client associated", sock);
-		return -1;
-	}
-	
+#if 0
 	if (client->buflen + bytes > (int)sizeof(client->msgbuf))
 	{
 		dbg_print("clientsock", "(%d) error: buffer size exceeded", sock);
@@ -163,23 +124,78 @@ int client_handle(socktype sock)
 		// parse and execute all commands in queue
 		while (client_parsebuffer(client));
 	}
+#endif
 	
 	return bytes;
 }
 
+int connectsock_create(const char *server, unsigned int port)
+{
+	int connectfd;
+	socktype sock;
+	struct sockaddr_in addr;
+
+	if ((connectfd = socket_create(PF_INET, SOCK_STREAM, 0)) == -1)
+	{
+		dbg_print("connectsock", "socket() failed (%d: %s)", errno, strerror(errno));
+		return -1;
+	}
+	
+	sock = connectfd;
+	
+	struct hostent *host_entry;
+	host_entry = gethostbyname(server);
+	
+	if(!host_entry)
+	{
+		dbg_print("connectsock", "gethostbyname() failed (%d: %s)", errno, strerror(errno));
+		return -2;
+	}
+	
+	memcpy(&addr.sin_addr.s_addr, host_entry->h_addr_list[0], host_entry->h_length);
+	addr.sin_family = host_entry->h_addrtype;
+	addr.sin_port = htons(port);
+	
+	socket_setnonblocking(sock);
+	
+	// FIXME: check for immediate error
+	socket_connect(sock, (struct sockaddr*)&addr, sizeof(addr));
+	
+#if 0
+	time_t timeout_start = time(NULL);
+	
+	int status;
+	
+	do
+	{
+		status = socket_connect(sock, (struct sockaddr*)&addr, sizeof(addr));
+		
+		if (status >= 0)
+			break;
+	}
+	while ((int)difftime(time(NULL), timeout_start) < CLIENT_CONNECT_TIMEOUT);
+	
+	if (status < 0)
+		return status;
+#endif
+	return sock;
+}
+
+int input_handle()
+{
+	const unsigned int max_input = 1024;
+	char input[max_input];
+	
+	fgets(input, max_input, stdin);
+	
+	dbg_print("input", "len=%d text=%s", (int)strlen(input), input);
+	
+	return 0;
+}
 
 int gameloop()
 {
-	if (snap_update)
-	{
-		if (snap_update & Foyer)
-		{
-			update_foyer_snapshot();
-			send_snapshot(Foyer);
-		}
-		
-		snap_update = 0x0;
-	}
+	
 	
 	return 0;
 }
@@ -191,76 +207,116 @@ int mainloop()
 	
 	fd_set fds;
 	
-	int listenfd;
-	if ((listenfd = listensock_create(DEFAULT_SERVER_PORT, 3)) < 0)
-	{
-		dbg_print("listensock", "(%d) error creating socket", sock);
-		return 1;
-	}
+	int connectfd;
+	connectfd = connectsock_create(server_address, DEFAULT_SERVER_PORT);
 	
-	sock = listenfd;
+	sock = connectfd;
+	
+	// check for connect
+	{
+		struct timeval timeout;  /* timeout for select */
+		timeout.tv_sec = 5;
+		timeout.tv_usec = 0;
+		
+		fd_set fds_write, fds_err;
+		
+		FD_ZERO(&fds_write);
+		FD_ZERO(&fds_err);
+		
+		FD_SET(sock, &fds_write);
+		FD_SET(sock, &fds_err);
+		
+		if (select(sock + 1, NULL, &fds_write, &fds_err, &timeout))
+		{
+			if (FD_ISSET(sock, &fds_err))
+			{
+				dbg_print("connectsock", "error connecting socket");
+				return -1;
+			}
+			
+			if (FD_ISSET(sock, &fds_write))
+			{
+				dbg_print("connectsock", "connected");
+			}
+		}
+	}
 	
 	for (;;)
 	{
+		// handle game
+		gameloop();
+		
 		struct timeval timeout;  /* timeout for select */
-		timeout.tv_sec = 1;
-		timeout.tv_usec = 0;
+		timeout.tv_sec = 0;
+		timeout.tv_usec = 50000;
 		
 		FD_ZERO(&fds);
 		
-		/* add listening socket to the fd-set */
+		/* add stdin and connected-socket to the fd-set */
+#if not defined(PLATFORM_WINDOWS)
+		FD_SET(STDIN_FILENO, &fds);
+#else	
+		static char inpBuf[1024] = "";
+		static unsigned int inpBufLen = 0;
+		
+		//http://api.farmanager.com/en/winapi/readconsoleinput.html
+		//http://api.farmanager.com/en/winapi/input_record.html
+		//http://api.farmanager.com/en/winapi/key_event_record.html
+		
+		HANDLE hStdin = GetStdHandle(STD_INPUT_HANDLE);
+		if (WaitForSingleObject(hStdin, 10) != WAIT_TIMEOUT)
+		{
+			
+			char buf;
+			//ReadFile(GetStdHandle(STD_INPUT_HANDLE), &buf, 1, 0, NULL );
+			INPUT_RECORD inr;
+			DWORD numRead;
+			ReadConsoleInput( hStdin, &inr, 1, &numRead );
+			//ReadConsole(hStdin, &buf, 1, &numRead, 0);
+			
+			if (inr.EventType == KEY_EVENT && inr.Event.KeyEvent.bKeyDown)
+			{
+				buf = inr.Event.KeyEvent.uChar.AsciiChar;
+				//dbg_print("windows", "got input: %c 0x%x", buf, buf);
+				
+				if (buf == '\r')
+				{
+					inpBuf[inpBufLen] = '\0';
+					printf("\r\n");
+					dbg_print("windows", "text: _%s_", inpBuf);
+					inpBufLen = 0;
+				}
+				else
+				{
+					if (buf /*!inr.Event.KeyEvent.dwControlKeyState*/)
+					{
+						printf("%c", buf);
+						inpBuf[inpBufLen++] = buf;
+					}
+				}
+			}
+		}
+#endif
 		FD_SET(sock, &fds);
 		max = sock;
 		
-		/* add control clients to select-SET */
-		for (vector<clientcon>::iterator e = clients.begin(); e != clients.end(); e++)
-		{
-			socktype client_sock = e->sock;
-			FD_SET(client_sock, &fds);
-			
-			if (client_sock > max)
-				max = client_sock;
-		}
-
 		// are there any modified descriptors?
 		if (select(max + 1, &fds, NULL, NULL, &timeout))
 		{
-			// listen socket
+#if not defined(PLATFORM_WINDOWS)
+			if (FD_ISSET(STDIN_FILENO, &fds))
+				input_handle();
+#endif
+			
 			if (FD_ISSET(sock, &fds))
 			{
-				socktype client_sock = socket_accept(sock, NULL, 0);
-				dbg_print("listensock", "(%d) accepted connection", client_sock);
-				
-				socket_setnonblocking(client_sock);
-				
-				// TODO: handle server-full
-				client_add(client_sock);
-				snap_update |= Foyer;
-				
-				FD_CLR(sock, &fds);
-				
-				// send initial data
-				char msg[1024];
-				snprintf(msg, sizeof(msg), "PSERVER %d %d",
-					VERSION_CREATE(VERSION_MAJOR, VERSION_MINOR, VERSION_REVISION),
-					client_sock);
-				send_msg(client_sock, msg);
-			}
-			else
-			{
-				// handle only one client-request per iteration
-				socktype sender = fdset_get_descriptor(&fds);
-				int status = client_handle(sender);
+				int status = server_handle(sock);
 				if (status <= 0)
 				{
-					dbg_print("clientsock", "(%d) closed connection (%d: %s)", sender, errno, strerror(errno));
-					socket_close(sender);
+					dbg_print("connectsock", "(%d) closed connection (%d: %s)", sock, errno, strerror(errno));
+					socket_close(sock);
 					
-					// remove client from vector
-					client_remove(sender);
-					snap_update |= Foyer;
-					
-					FD_CLR(sender, &fds);
+					return -1;
 				}
 			}
 		}
@@ -269,9 +325,6 @@ int mainloop()
 			// select-timeout occured
 			//fprintf(stderr, ".\n"); fflush(stderr);
 		}
-		
-		// handle game
-		gameloop();
 	}
 	
 	return 0;
@@ -279,15 +332,25 @@ int mainloop()
 
 int main(int argc, char **argv)
 {
-	dbg_print("main", "client version %d.%d.%d",
+	dbg_print("main", "HoldingNuts pclient_t version %d.%d.%d",
 		VERSION_MAJOR, VERSION_MINOR, VERSION_REVISION);
 	
 #if not defined(PLATFORM_WINDOWS)
 	// ignore broken-pipe signal eventually caused by sockets
 	signal(SIGPIPE, SIG_IGN);
 #endif
+
+#if defined(PLATFORM_WINDOWS)
+	//SetConsoleMode(GetStdHandle(STD_INPUT_HANDLE),ENABLE_LINE_INPUT|ENABLE_ECHO_INPUT);
+#endif
 	
 	network_init();
+	
+	// first arg is the hostname
+	if (argc < 2)
+		strcpy(server_address, "localhost");
+	else
+		snprintf(server_address, sizeof(server_address), "%s", argv[1]);
 	
 	mainloop();
 	
