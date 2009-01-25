7116c60b3704ccec4317a1a50062f7b210ec91eb
Dominik D. Geyer <dominik.geyer@holdingnuts.net>
2009-01-25 21:35:10 +0000

    implemented simple config-parser; fixed abandoned of r222
    git-svn-id: http://svn.code.sf.net/p/holdingnuts/code/trunk@223 00c8e4f2-bc05-11dd-9892-7df5a783e2f7

---
+++ src/system/SysAccess.h
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2008, 2009, Dominik Geyer
+ *
+ * This file is part of HoldingNuts.
+ *
+ * HoldingNuts is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * HoldingNuts is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with HoldingNuts.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authors:
+ *     Dominik Geyer <dominik.geyer@holdingnuts.net>
+ */
+
+
+#ifndef _SYSACCESS_H
+#define _SYSACCESS_H
+
+#include "Platform.h"
+
+#include <stdio.h>
+#include <string.h>
+
+
+#if defined __cplusplus
+        extern "C" {
+#endif
+
+
+typedef FILE filetype;
+
+typedef enum {
+	mode_read	= 0x01,
+	mode_write	= 0x02
+} filemode;
+
+typedef enum {
+	seek_set	= 0x01,
+	seek_cur	= 0x02,
+	seek_end	= 0x04
+} seekpos;
+
+
+filetype* file_open(const char *filename, int mode);
+int file_close(filetype *fp);
+size_t file_read(filetype *fp, void *buf, unsigned int size);
+size_t file_write(filetype *fp, void *buf, unsigned int size);
+int file_setpos(filetype *fp, long offset, int whence);
+long file_getpos(filetype *fp);
+long file_length(filetype *fp);
+
+char* file_readline(filetype *fp, char *buf, int max);
+int file_writeline(filetype *fp, char *buf);
+
+#if defined __cplusplus
+    }
+#endif
+
+#endif /* _SYSACCESS_H */
