6918a26087aa1382bca5c83824303a10792a3edd
Dominik D. Geyer <dominik.geyer@holdingnuts.net>
2009-02-16 22:56:26 +0000

    updated german translation
    git-svn-id: http://svn.code.sf.net/p/holdingnuts/code@330 00c8e4f2-bc05-11dd-9892-7df5a783e2f7

--- trunk/src/client/translations/hn_de.ts
+++ trunk/src/client/translations/hn_de.ts
@@ -4,98 +4,203 @@
 <context>
     <name>PClient</name>
     <message>
-        <location filename="pclient.cpp" line="252"/>
+        <location filename="../pclient.cpp" line="560"/>
         <source>Connecting...</source>
         <translation>Verbinde...</translation>
     </message>
     <message>
-        <location filename="pclient.cpp" line="334"/>
+        <location filename="../pclient.cpp" line="779"/>
         <source>Connection closed.</source>
         <translation>Verbindung geschlossen.</translation>
     </message>
     <message>
-        <location filename="pclient.cpp" line="296"/>
-        <source>Connection timeout.</source>
-        <translation>Zeitüberschreitung bei Verbindung.</translation>
-    </message>
-    <message>
-        <location filename="pclient.cpp" line="309"/>
+        <location filename="../pclient.cpp" line="758"/>
         <source>Connected.</source>
         <translation>Verbunden.</translation>
+    </message>
+    <message>
+        <location filename="../pclient.cpp" line="173"/>
+        <source>Game (%1) has been started.</source>
+        <translation>Spiel (%1) wurde gestartet.</translation>
+    </message>
+    <message>
+        <location filename="../pclient.cpp" line="197"/>
+        <source>Game (%1) has been ended.</source>
+        <translation>Spiel (%1) wurde beendet.</translation>
+    </message>
+    <message>
+        <location filename="../pclient.cpp" line="748"/>
+        <source>Connection error: %1.</source>
+        <translation>Verbindungsfehler: %1.</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsDialog</name>
+    <message>
+        <location filename="../SettingsDialog.cpp" line="34"/>
+        <source>Settings</source>
+        <translation>Einstellungen</translation>
+    </message>
+    <message>
+        <location filename="../SettingsDialog.cpp" line="40"/>
+        <source>General</source>
+        <translation>Allgemein</translation>
+    </message>
+    <message>
+        <location filename="../SettingsDialog.cpp" line="41"/>
+        <source>Appearance</source>
+        <translation>Aussehen</translation>
+    </message>
+    <message>
+        <location filename="../SettingsDialog.cpp" line="56"/>
+        <source>Player name</source>
+        <translation>Spielername</translation>
+    </message>
+    <message>
+        <location filename="../SettingsDialog.cpp" line="59"/>
+        <source>UUID</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../SettingsDialog.cpp" line="70"/>
+        <source>Log to file</source>
+        <translation>Log in Datei</translation>
+    </message>
+    <message>
+        <location filename="../SettingsDialog.cpp" line="74"/>
+        <source>Locale</source>
+        <translation>Sprache</translation>
+    </message>
+    <message>
+        <location filename="../SettingsDialog.cpp" line="82"/>
+        <source>Auto-Detect</source>
+        <translation>Automatisch</translation>
+    </message>
+    <message>
+        <location filename="../SettingsDialog.cpp" line="108"/>
+        <source>Show strength of hand</source>
+        <translation>Handstärke anzeigen</translation>
     </message>
 </context>
 <context>
     <name>WMain</name>
     <message>
-        <location filename="WMain.cpp" line="34"/>
+        <location filename="../WMain.cpp" line="157"/>
         <source>Error connecting.</source>
         <translation>Fehler beim Verbinden.</translation>
     </message>
     <message>
-        <location filename="WMain.cpp" line="108"/>
+        <location filename="../WMain.cpp" line="50"/>
         <source>Connection</source>
         <translation>Verbindung</translation>
     </message>
     <message>
-        <location filename="WMain.cpp" line="110"/>
+        <location filename="../WMain.cpp" line="57"/>
         <source>Connect</source>
         <translation>Verbinden</translation>
     </message>
     <message>
-        <location filename="WMain.cpp" line="114"/>
+        <location filename="../WMain.cpp" line="61"/>
         <source>Close</source>
         <translation>Trennen</translation>
     </message>
     <message>
-        <location filename="WMain.cpp" line="137"/>
-        <source>Foyer chat</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="WMain.cpp" line="142"/>
-        <source>Chat</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="WMain.cpp" line="159"/>
+        <location filename="../WMain.cpp" line="92"/>
         <source>Test</source>
         <translation type="unfinished"></translation>
     </message>
-</context>
-<context>
-    <name>WSeat</name>
     <message>
-        <location filename="WTable.cpp" line="124"/>
-        <source>Seat</source>
+        <location filename="../WMain.cpp" line="45"/>
+        <source>HoldingNuts Foyer</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="WTable.cpp" line="126"/>
-        <source>123.45</source>
+        <location filename="../WMain.cpp" line="52"/>
+        <source>Host:</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="WTable.cpp" line="128"/>
-        <source>Action</source>
+        <location filename="../WMain.cpp" line="82"/>
+        <source>Register</source>
+        <translation>Anmelden</translation>
+    </message>
+    <message>
+        <location filename="../WMain.cpp" line="95"/>
+        <source>Settings</source>
+        <translation>Einstellungen</translation>
+    </message>
+    <message>
+        <location filename="../WMain.cpp" line="98"/>
+        <source>Foyer Chat</source>
         <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../WMain.cpp" line="128"/>
+        <source>Error</source>
+        <translation>Fehler</translation>
     </message>
 </context>
 <context>
     <name>WTable</name>
     <message>
-        <location filename="WTable.cpp" line="82"/>
+        <location filename="../WTable.cpp" line="552"/>
         <source>Fold</source>
-        <translation>Passen</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="WTable.cpp" line="84"/>
-        <source>Check/Call</source>
-        <translation>Schieben/Mitgehen</translation>
+        <location filename="../WTable.cpp" line="274"/>
+        <source>HoldingNuts table</source>
+        <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="WTable.cpp" line="86"/>
-        <source>Bet/Raise</source>
-        <translation>Setzen/Erhöhen</translation>
+        <location filename="../WTable.cpp" line="290"/>
+        <source>Show</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../WTable.cpp" line="293"/>
+        <source>Muck</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../WTable.cpp" line="296"/>
+        <source>I&apos;m back</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../WTable.cpp" line="299"/>
+        <source>Sitout</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../WTable.cpp" line="557"/>
+        <source>Call</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../WTable.cpp" line="517"/>
+        <source>Raise</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../WTable.cpp" line="562"/>
+        <source>Check</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../WTable.cpp" line="519"/>
+        <source>Bet</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../WTable.cpp" line="555"/>
+        <source>Allin</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../WTable.cpp" line="561"/>
+        <source>Check/Fold</source>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 </TS>
