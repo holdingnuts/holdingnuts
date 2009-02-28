6339167808cc88d4188ce608bd64f3940e2f3742
Dominik D. Geyer <dominik.geyer@holdingnuts.net>
2009-02-28 01:51:59 +0000

    merged branch sdl-audio back into trunk
    git-svn-id: http://svn.code.sf.net/p/holdingnuts/code@385 00c8e4f2-bc05-11dd-9892-7df5a783e2f7

---
+++ trunk/src/client/Audio.h
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2008, 2009, Dominik Geyer
+ *
+ * This file is part of HoldingNuts.
+ *
+ * HoldingNuts is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * HoldingNuts is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with HoldingNuts.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authors:
+ *     Dominik Geyer <dominik.geyer@holdingnuts.net>
+ */
+
+
+#ifndef _AUDIO_H
+#define _AUDIO_H
+
+#if defined __cplusplus
+        extern "C" {
+#endif
+
+int audio_init();
+int audio_deinit();
+int audio_load(unsigned int id, const char *file);
+void audio_play(unsigned int id);
+
+#if defined __cplusplus
+    }
+#endif
+
+#endif /* _AUDIO_H */
