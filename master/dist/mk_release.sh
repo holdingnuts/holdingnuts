021ab5b42781712b2cb2a88959c405ccc7805b55
Dominik D. Geyer <dominik.geyer@holdingnuts.net>
2009-03-15 20:09:12 +0000

    created script for creating release packages
    git-svn-id: http://svn.code.sf.net/p/holdingnuts/code@457 00c8e4f2-bc05-11dd-9892-7df5a783e2f7

---
+++ master/dist/mk_release.sh
@@ -0,0 +1,96 @@
+#!/bin/bash
+#
+# Configure and build the sources with: cmake -DCMAKE_BUILD_TYPE=Release -DUSE_SVNREV=On
+# and build the translations
+
+if [[ $# < 4 ]] ; then
+	echo "mk_release.sh <trunk-dir> <build-dir> <platform> <version>" >&2
+	exit 0
+fi
+
+BUILD=$1
+TRUNK=$2
+PLATFORM=$3
+RELVER=$4
+
+if [[ ! -d ${BUILD} || ! -d ${TRUNK} ]] ; then
+	echo "Error: Build/Trunk dir does not exist." >&2
+	exit 1
+fi
+
+if [[ ${PLATFORM} != linux_x86 && ${PLATFORM} != linux_x86_64 && ${PLATFORM} != win32 ]] ; then
+	echo "Error: Unsupported platform: ${PLATFORM}" >&2
+	exit 1
+fi
+
+case ${PLATFORM} in
+win* )
+	convert_text=1
+	is_windows=1
+	;;
+esac
+
+PKGSTR="holdingnuts-${RELVER}-${PLATFORM}"
+TMPDIR=$PKGSTR
+
+echo "--- mk_release.sh running for ${PKGSTR} ---"
+
+
+if [[ -d ${TMPDIR} ]] ; then
+	echo "Deleting old temporary working directory..."
+	rm -rf ${TMPDIR}
+fi
+
+
+echo "Creating temporary working directory ${TMPDIR}..."
+mkdir ${TMPDIR}
+
+if [[ -n ${convert_text} ]] ; then
+	echo "Copying and converting license file..."
+	awk '{ sub(/$/,"\r"); print }' ${TRUNK}/LICENSE > ${TMPDIR}/LICENSE.txt    # convert CRLF to LF
+else
+	echo "Copying license file..."
+	cp ${TRUNK}/LICENSE ${TMPDIR}/LICENSE
+fi
+
+
+echo "Copying data-dir..."
+cp -r -t ${TMPDIR} ${TRUNK}/data
+
+
+echo "Copying binaries..."
+if [[ -n $is_windows ]] ; then
+	cp -t ${TMPDIR} ${BUILD}/src/client/pclient.exe
+	cp -t ${TMPDIR} ${BUILD}/src/server/pserver.exe
+else
+	cp -t ${TMPDIR} ${BUILD}/src/client/pclient
+	cp -t ${TMPDIR} ${BUILD}/src/server/pserver
+	
+	# wrapper script
+	cp -t ${TMPDIR} files/pclient.sh
+fi
+
+echo "Copying translations..."
+cp -r -t  ${TMPDIR}/data/i18n ${BUILD}/src/client/*.qm
+
+if [[ -z $is_windows ]] ; then
+	echo "Creating empty lib directory"
+	mkdir ${TMPDIR}/lib
+fi
+
+echo "Copying shortcuts..."
+if [[ -n $is_windows ]] ; then
+	cp -t ${TMPDIR} files/HoldingNuts.net.url
+	cp -t ${TMPDIR} files/config_directory.lnk
+else
+	cp -t ${TMPDIR} files/HoldingNuts.net.desktop
+fi
+
+echo "Copying logo..."
+cp ${TRUNK}/src/client/res/hn_logo.png ${TMPDIR}/HoldingNuts.png
+
+echo "Removing svn entries..."
+rm -rf $(find ${TMPDIR} -type d -name '.svn')
+
+echo " * Do not forget to copy the qt_translations (e.g. qt_de.qm) into ${TMPDIR}/data/i18n as well"
+echo " * Do not forget to copy the libaries (windows: *.dll into ${TMPDIR} ; linux: *.so into ${TMPDIR}/lib"
