1062209d5d54af645e481c42f1c069d63aa53dc1
Dominik D. Geyer <dominik.geyer@holdingnuts.net>
2009-03-16 21:19:09 +0000

    release-script: added support for source-release packages
    git-svn-id: http://svn.code.sf.net/p/holdingnuts/code@462 00c8e4f2-bc05-11dd-9892-7df5a783e2f7

--- master/dist/mk_release.sh
+++ master/dist/mk_release.sh
@@ -1,40 +1,78 @@
 #!/bin/bash
-#
-# Configure and build the sources with: cmake -DCMAKE_BUILD_TYPE=Release -DUSE_SVNREV=On
-# and build the translations
+# release script
 
-if [[ $# < 4 ]] ; then
+if [[ $# != 2 && $# != 4 ]] ; then
+	echo "mk_release.sh <trunk-dir> <version>" >&2
 	echo "mk_release.sh <trunk-dir> <build-dir> <platform> <version>" >&2
 	exit 0
+elif [[ $# == 2 ]] ; then
+	src_release=1
 fi
+
 
 TRUNK=$1
-BUILD=$2
-PLATFORM=$3
-RELVER=$4
 
-if [[ ! -d ${BUILD} || ! -d ${TRUNK} ]] ; then
-	echo "Error: Build/Trunk dir does not exist." >&2
+if [[ ${src_release} ]] ; then
+	RELVER=$2
+else
+	BUILD=$2
+	PLATFORM=$3
+	RELVER=$4
+fi
+
+if [[ ! -d ${TRUNK} ]] ; then
+	echo "Error: Trunk dir does not exist." >&2
 	exit 1
 fi
 
-if [[ ${PLATFORM} != linux_x86 && ${PLATFORM} != linux_x86_64 && ${PLATFORM} != win32 ]] ; then
-	echo "Error: Unsupported platform: ${PLATFORM}" >&2
-	exit 1
+if [[ ! ${src_release} ]] ; then
+	if [[ ! -d ${BUILD} ]] ; then
+		echo "Error: Build dir does not exist." >&2
+		exit 1
+	fi
+
+	if [[ ${PLATFORM} != linux_x86 && ${PLATFORM} != linux_x86_64 && ${PLATFORM} != win32 ]] ; then
+		echo "Error: Unsupported platform: ${PLATFORM}" >&2
+		exit 1
+	fi
+
+	case ${PLATFORM} in
+	win* )
+		convert_text=1
+		is_windows=1
+		;;
+	esac
+	
+	PKGSTR="holdingnuts-${RELVER}-${PLATFORM}"
+else
+	PKGSTR="holdingnuts-${RELVER}-src"
 fi
 
-case ${PLATFORM} in
-win* )
-	convert_text=1
-	is_windows=1
-	;;
-esac
 
-PKGSTR="holdingnuts-${RELVER}-${PLATFORM}"
+
 TMPDIR=$PKGSTR
 
 echo "--- mk_release.sh running for ${PKGSTR} ---"
 
+if [[ ! ${src_release} ]] ; then
+	echo
+	echo "IMPORTANT:"
+	echo "Configure and build the sources with"
+	echo "  cmake -DCMAKE_BUILD_TYPE=Release -DUSE_SVNREV=On"
+	echo "and build the translations with"
+	echo "  make translations"
+	echo "If this wasn't done, the release package will be incomplete!"
+fi
+
+echo
+echo "Checking for unmodified trunk..."
+SVNREV=$(svnversion ${TRUNK})
+echo "SVN revision: ${SVNREV}"
+svn st --no-ignore ${TRUNK}
+
+echo
+echo "--- Verify and press ENTER to continue ---"
+read
 
 if [[ -d ${TMPDIR} ]] ; then
 	echo "Deleting old temporary working directory..."
@@ -45,52 +83,65 @@ fi
 echo "Creating temporary working directory ${TMPDIR}..."
 mkdir ${TMPDIR}
 
-if [[ -n ${convert_text} ]] ; then
-	echo "Copying and converting license file..."
-	awk '{ sub(/$/,"\r"); print }' ${TRUNK}/LICENSE > ${TMPDIR}/LICENSE.txt    # convert CRLF to LF
-else
-	echo "Copying license file..."
-	cp ${TRUNK}/LICENSE ${TMPDIR}/LICENSE
-fi
+
+if [[ ! ${src_release} ]] ; then
+	if [[ ${convert_text} ]] ; then
+		echo "Copying and converting license file..."
+		awk '{ sub(/$/,"\r"); print }' ${TRUNK}/LICENSE > ${TMPDIR}/LICENSE.txt    # convert CRLF to LF
+	else
+		echo "Copying license file..."
+		cp ${TRUNK}/LICENSE ${TMPDIR}/LICENSE
+	fi
 
 
-echo "Copying data-dir..."
-cp -r -t ${TMPDIR} ${TRUNK}/data
+	echo "Copying data-dir..."
+	cp -r -t ${TMPDIR} ${TRUNK}/data
 
 
-echo "Copying binaries..."
-if [[ -n $is_windows ]] ; then
-	cp -t ${TMPDIR} ${BUILD}/src/client/holdingnuts.exe
-	cp -t ${TMPDIR} ${BUILD}/src/server/holdingnuts-server.exe
-else
-	cp -t ${TMPDIR} ${BUILD}/src/client/holdingnuts
-	cp -t ${TMPDIR} ${BUILD}/src/server/holdingnuts-server
+	echo "Copying binaries..."
+	if [[ $is_windows ]] ; then
+		cp -t ${TMPDIR} ${BUILD}/src/client/holdingnuts.exe
+		cp -t ${TMPDIR} ${BUILD}/src/server/holdingnuts-server.exe
+	else
+		cp -t ${TMPDIR} ${BUILD}/src/client/holdingnuts
+		cp -t ${TMPDIR} ${BUILD}/src/server/holdingnuts-server
+		
+		# wrapper script
+		cp -t ${TMPDIR} files/holdingnuts.sh
+	fi
+
+	echo "Copying translations..."
+	cp -r -t  ${TMPDIR}/data/i18n ${BUILD}/src/client/*.qm
+
+	if [[ ! $is_windows ]] ; then
+		echo "Creating empty lib directory"
+		mkdir ${TMPDIR}/lib
+	fi
+
+	echo "Copying shortcuts..."
+	if [[ $is_windows ]] ; then
+		cp -t ${TMPDIR} files/HoldingNuts.net.url
+		cp -t ${TMPDIR} files/config_directory.lnk
+	else
+		cp -t ${TMPDIR} files/HoldingNuts.net.desktop
+	fi
+
+	echo "Copying logo..."
+	cp ${TRUNK}/src/client/res/hn_logo.png ${TMPDIR}/holdingnuts.png
 	
-	# wrapper script
-	cp -t ${TMPDIR} files/holdingnuts.sh
-fi
-
-echo "Copying translations..."
-cp -r -t  ${TMPDIR}/data/i18n ${BUILD}/src/client/*.qm
-
-if [[ -z $is_windows ]] ; then
-	echo "Creating empty lib directory"
-	mkdir ${TMPDIR}/lib
-fi
-
-echo "Copying shortcuts..."
-if [[ -n $is_windows ]] ; then
-	cp -t ${TMPDIR} files/HoldingNuts.net.url
-	cp -t ${TMPDIR} files/config_directory.lnk
+	echo "!!! Do not forget to"
+	echo " * copy the qt_translations (e.g. qt_de.qm) into ${TMPDIR}/data/i18n as well"
+	echo " * copy the libaries (windows: *.dll into ${TMPDIR} ; linux: *.so into ${TMPDIR}/lib"
 else
-	cp -t ${TMPDIR} files/HoldingNuts.net.desktop
+	echo "Copying trunk..."
+	cp -r -t ${TMPDIR} ${TRUNK}/*
+	
+	echo "Touching svn revision info file..."
+	touch ${TMPDIR}/svn_r${SVNREV}
 fi
 
-echo "Copying logo..."
-cp ${TRUNK}/src/client/res/hn_logo.png ${TMPDIR}/HoldingNuts.png
 
 echo "Removing svn entries..."
 rm -rf $(find ${TMPDIR} -type d -name '.svn')
 
-echo " * Do not forget to copy the qt_translations (e.g. qt_de.qm) into ${TMPDIR}/data/i18n as well"
-echo " * Do not forget to copy the libaries (windows: *.dll into ${TMPDIR} ; linux: *.so into ${TMPDIR}/lib"
+echo "Finished."
