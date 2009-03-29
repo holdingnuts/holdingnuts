f9469ea0de1ffc6968633179116ebb3713fb0def
Dominik D. Geyer <dominik.geyer@holdingnuts.net>
2009-03-29 22:17:04 +0000

    removed tags for test-sessions
    git-svn-id: http://svn.code.sf.net/p/holdingnuts/code@500 00c8e4f2-bc05-11dd-9892-7df5a783e2f7

--- tags/test10/build_windows.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cmake -DCMAKE_TOOLCHAIN_FILE=$(pwd)/Toolchain_Windows.cmake -DWIN32:BOOL=On .
-make #VERBOSE=1
-
--- tags/test3/distclean.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# clean all temporary cmake-files
-
-make clean   2>/dev/null
-rm -r {.,src}/{,network,libpoker,server,client,test,system}/{Makefile,CMakeCache.txt,CMakeFiles,cmake_install.cmake}   2>/dev/null
