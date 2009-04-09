83302d6190022420ca56558d6564f6e751aabe2b
Dominik D. Geyer <dominik.geyer@holdingnuts.net>
2009-04-09 21:33:58 +0000

    master: fixed position of suits and made them slightly bigger; updated card decks
    git-svn-id: http://svn.code.sf.net/p/holdingnuts/code@542 00c8e4f2-bc05-11dd-9892-7df5a783e2f7

--- master/game/deck/compose_cards.sh
+++ master/game/deck/compose_cards.sh
@@ -16,8 +16,8 @@
 #
 #  You should have received a copy of the GNU General Public License
 #  along with HoldingNuts.  If not, see <http://www.gnu.org/licenses/>.
-
-# Usage: compose_cards.sh [4color]
+#
+# Usage: compose_cards.sh < 2 | 4 >
 
 
 density=80
@@ -25,8 +25,8 @@ font=Arial  #Eras-Normal  #Baskerville-Normal
 font_ratio=35
 font_kerning=-4
 
-ratio_suit_small=28
-ratio_suit_big=47
+ratio_suit_small=34
+ratio_suit_big=50
 
 color_s='rgb(0,0,0)'
 color_h='rgb(212,0,0)'
@@ -96,21 +96,29 @@ create_card()
 		name=$1
 	fi
 
+	# big suit middle
 	sb_w=$(img_width tmp/suit_${2}_big.png)
 	sb_h=$(img_height tmp/suit_${2}_big.png)
 	sb_x=$((tw / 2 - sb_w / 2))
 	sb_y=$((th / 2 - sb_h / 2))
 
+	# small suit top right
 	s1s_w=$(img_width tmp/suit_${2}.png)
-	s1s_x=$((tw - s1s_w * 2))
-	s1s_y=$((s1s_w))
-	s2s_x=$((s1s_w))
-	s2s_y=$((th - s1s_w * 2))
+	s1s_h=$(img_height tmp/suit_${2}.png)
+	s1s_x=$((tw - s1s_w - s1s_w / 2))
+	s1s_y=$((s1s_h - s1s_h / 2))
+	
+	# small suit bottom left
+	s2s_x=$((s1s_w - s1s_w / 2))
+	s2s_y=$((th - s1s_h - s1s_h / 2))
 
+	# face top left
 	f1_w=$(img_width tmp/face_${1}.png)
 	f1_h=$(img_height tmp/face_${1}.png)
 	f1_x=$((f1_h / 4))
 	f1_y=$((f1_h / 8))
+	
+	# flipped face bottom right
 	f2_x=$((tw - f1_w - f1_x))
 	f2_y=$((th - f1_h - f1_y))
 
@@ -147,22 +155,36 @@ create_blank_card()
 
 ################################################################################
 
-if [[ $1 == "4color" ]] ; then
+if [[ $# != 1 ]]; then
+	echo "Usage: compose_cards.sh < 2 | 4 >" >&2
+	exit
+fi
+
+
+decktype=$1
+
+if [[ ${decktype} == "4" ]] ; then
 	echo "Creating 4-color deck"
 	four_color=1
-else
+elif [[ ${decktype} == "2" ]] ; then
 	echo "Creating 2-color deck"
 	
 	# use black/red for diamonds and clubs
 	color_d="${color_h}"
 	color_c="${color_s}"
+else
+	echo "Unknown deck type." >&2
+	exit 1
 fi
 
 
 mkdir -p tmp output
 
+# create the card surface
 create_template
 
+
+# get card surface width and height
 tw=$(img_width tmp/card.png)
 th=$(img_height tmp/card.png)
 
