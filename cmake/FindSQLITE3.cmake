76efdb4cab89b5e7cd2fd38cd8580f6189913380
Dominik D. Geyer <dominik.geyer@holdingnuts.net>
2009-09-07 02:10:38 +0000

    rewrite of CMake module FindSQLITE3
    git-svn-id: http://svn.code.sf.net/p/holdingnuts/code/branches/sqlite@714 00c8e4f2-bc05-11dd-9892-7df5a783e2f7

--- cmake/FindSQLITE3.cmake
+++ cmake/FindSQLITE3.cmake
@@ -1,42 +1,52 @@
-# - Try to find SQLITE3
-# Once done this will define
-#
-#  SQLITE3_FOUND - system has SQLITE3
-#  SQLITE3_INCLUDE_DIR - the SQLITE3 include directory
-#  SQLITE3_LIBRARIES - Link these to use SQLITE3
-#  SQLITE3_DEFINITIONS - Compiler switches required for using SQLITE3
-# Redistribution and use is allowed according to the terms of the BSD license.
-# For details see the accompanying COPYING-CMAKE-SCRIPTS file.
-#
+# Locate SQLITE3
+# This module defines
+#  SQLITE3_FOUND, SQLITE3 is found
+#  SQLITE3_INCLUDE_DIR, the SQLITE3 include directory
+#  SQLITE3_LIBRARIES - the SQLITE3 libraries
 
 
-if ( SQLITE3_INCLUDE_DIR AND SQLITE3_LIBRARIES )
-   # in cache already
+if (SQLITE3_INCLUDE_DIR AND SQLITE3_LIBRARIES)
+   # in cache already, be quiet
    SET(SQLITE3_FIND_QUIETLY TRUE)
-endif ( SQLITE3_INCLUDE_DIR AND SQLITE3_LIBRARIES )
+endif (SQLITE3_INCLUDE_DIR AND SQLITE3_LIBRARIES)
 
-# use pkg-config to get the directories and then use these values
-# in the FIND_PATH() and FIND_LIBRARY() calls
-if( NOT WIN32 )
-  INCLUDE(FindPkgConfig)
+FIND_PATH(SQLITE3_INCLUDE_DIR sqlite.h
+  HINTS
+  $ENV{SQLITE3_DIR}
+  PATH_SUFFIXES include/sqlite3 include/sqlite include
+  PATHS
+  ~/Library/Frameworks
+  /Library/Frameworks
+  /usr/local
+  /usr
+  /sw # Fink
+  /opt/local # DarwinPorts
+  /opt/csw # Blastwave
+  /opt
+)
 
-  pkg_check_modules(SQLITE3 REQUIRED sqlite3 )
+#message("SQLITE3 include dir: ${SQLITE3_INCLUDE_DIR}")
 
-endif( NOT WIN32 )
+FIND_LIBRARY(SQLITE3_LIBRARIES
+  NAMES sqlite3
+  HINTS
+  $ENV{SQLITE3_DIR}
+  PATH_SUFFIXES lib64 lib
+  PATHS
+  ~/Library/Frameworks
+  /Library/Frameworks
+  /usr/local
+  /usr
+  /sw
+  /opt/local
+  /opt/csw
+  /opt
+)
 
-#FIND_PATH(SQLITE3_INCLUDE_DIR NAMES sqlite3.h
-#  PATHS
-#  ${_SQLITE3IncDir}
-#)
+#message("SQLITE3 libs: ${SQLITE3_LIBRARIES}")
 
-#FIND_LIBRARY(SQLITE3_LIBRARIES NAMES sqlite3
-#  PATHS
-#  ${_SQLITE3LinkDir}
-#)
+INCLUDE(FindPackageHandleStandardArgs)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(SQLITE3 DEFAULT_MSG SQLITE3_INCLUDE_DIR SQLITE3_LIBRARIES )
 
-#include(FindPackageHandleStandardArgs)
-#FIND_PACKAGE_HANDLE_STANDARD_ARGS(SQLITE3 DEFAULT_MSG SQLITE3_INCLUDE_DIR SQLITE3_LIBRARIES )
-
-# show the SQLITE3_INCLUDE_DIR and SQLITE3_LIBRARIES variables only in the advanced view
 MARK_AS_ADVANCED(SQLITE3_INCLUDE_DIR SQLITE3_LIBRARIES )
 
